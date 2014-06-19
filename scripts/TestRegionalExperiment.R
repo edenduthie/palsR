d60a8a889ef988a7b839fbf2bb61a1050ad3cec5
Gab Abramowitz <gabsun@gmail.com>
2014-06-19 13:35:58 +1000

    Bug fixes after renaming
    Renaming from last commit was incomplete, now test cases run.

--- scripts/TestRegionalExperiment.R
+++ scripts/TestRegionalExperiment.R
@@ -1,7 +1,7 @@
 library("RJSONIO")
 inputFile <- "TestInput_Regional.json"
 input <- fromJSON(paste(readLines(inputFile), collapse=""));
-Rruntime = system.time(source("Aus0.25Experiment.R"))
+Rruntime = system.time(source("RegionalAus0.25Experiment.R"))
 print(paste('Time to run:',Rruntime[3]))
 output <- toJSON(output)
 fileConn<-file("output_Regional.json")
